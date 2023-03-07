04c13657ba6fb55b721b59c5097cfd931bdff2ac
unknown <sagarmikeylevi@gmail.com>
2023-03-07 18:07:58 +0530

    Added the payload Schema

---
+++ model/Payload.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const payloadSchema = new Schema({
+    productID: {
+        type: String,
+        required: true
+    },
+    quantity: {
+        type: String,
+        required: true
+    },
+    operation: {
+        type: String,
+        required: true
+    }
+},{
+    timestamps: true
+});
+
+const Payload = mongoose.model('Payload' , payloadSchema);
+module.exports = Payload;
